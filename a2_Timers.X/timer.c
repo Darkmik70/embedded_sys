1ccb724bf034fd8eacff3d4b4510141f00f1bb04
Michał <mkrepa117@outlook.com>
2024-04-15 17:47:26 +0200

    Update timer.c and timer.h

--- a2_Timers.X/timer.c
+++ a2_Timers.X/timer.c
@@ -8,3 +8,131 @@
 
 #include "xc.h"
 #include "timer.h"
+
+
+void tmr_setup_period(int timer, int ms)
+{
+    long clock = 72000000;      // 72 MHz
+    int ps_mode = 0;            // Mode of prescaler 0, 1, 2, 3
+    long new_clock;             // Prescaled clock
+
+    /* check if we need to use prescaler */
+    if (ms * clock > 65535) // Check if the number of steps is within thershold of 16bit
+    {
+        // too many steps, use prescaler
+        new_clock = clock / 8; // 1:8 
+        ps_mode = 1;
+        if (ms * new_clock > 65535) // Check again
+        {
+            new_clock = clock / 16; // 1:16
+            ps_mode = 2;
+            if (ms * new_clock > 65535) //and again
+            {
+                new_clock = clock / 256 // 1:256
+                ps_mode = 3;
+            }
+        }
+    }
+    /* Set the value for the timer period */
+    long result = (new_clocks)*((float) ms / 1000); // value of the timer
+    if (timer == TIMER1)
+    {
+        
+        T1CONbits.TON = 0;
+        IFS0bits.T1IF = 0;
+        TMR1 = 0;               // reset the timer counter
+        PR1 = result;           // set to compare the TMRx
+        
+        /* set the prescaler */
+        if (ps_mode == 3)
+        {
+            T1CONbits.TCKPS = 3;
+        }
+        else if (ps_mode == 2)
+        {
+            T1CONbits.TCKPS = 2;
+        }
+        else if (ps_mode == 1)
+        {
+            T1CONbits.TCKPS = 1;
+        }
+        else if (num == 0)
+        {
+            T1CONbits.TCKPS = 0;
+        }
+        /* Start the timer*/
+        T1CONbits.TON = 1; 
+    }                      
+    else if (timer == TIMER2)
+    {
+        T2CONbits.TON = 0;
+        IFS0bits.T2IF = 0;
+        TMR2 = 0;
+        PR2 = result; // set to compare the TMRx
+
+        /* set the prescaler */
+        if (num == 3)
+        {
+            T2CONbits.TCKPS = 3; 
+        }
+        else if (num == 2)
+        {
+            T2CONbits.TCKPS = 2; 
+        }
+        else if (num == 1)
+        {
+            T2CONbits.TCKPS = 1;
+        }
+        else if (num == 0)
+        {
+            T2CONbits.TCKPS = 0; 
+        }
+        /* Start the timer*/
+        T2CONbits.TON = 1;
+    }
+}
+
+int tmr_wait_period(int timer)
+{
+    if (timer == TIMER1)
+    {
+        T1CONbits.TON = 0;
+        // Check if the flag is already raised
+        if (IFS0bits.T1IF == 1)
+        {
+            IFS0bits.T1IF = 0;
+            return 1;
+        }
+        T1CONbits.TON = 1; // Start the timer
+        // Wait until flag is raised
+        while (IFS0bits.T1IF == 0)
+        {
+        }
+        IFS0bits.T1IF = 0; // zero the flag from 1 to 0 again.
+        return 0;
+    }
+    else if (timer == TIMER2)
+    {
+        T2CONbits.TON = 0;
+        // Check if the flag is already raised
+        if (IFS0bits.T2IF == 1)
+        {
+            IFS0bits.T2IF = 0;
+            return 1;
+        }
+        T2CONbits.TON = 1;  // Start the timer
+        // Wait until flag is raised
+        while (IFS0bits.T2IF == 0)
+        {
+        }
+        IFS0bits.T2IF = 0;  // zero the flag from 1 to 0 again.
+        return 0;
+    }
+}
+
+
+void tmr_wait_ms(int timer, int ms)
+{
+    tmr_setup_period(timer, ms);
+    tmr_wait_period(timer);
+}
--- a2_Timers.X/timer.h
+++ a2_Timers.X/timer.h
@@ -6,18 +6,29 @@
 #define TIMER1 1
 #define TIMER2 2
 
-// 
 /**
- * Setup the timer period. Must be only used once in the code
- * @param timer
- * @param ms
+ * @brief Setup the timer for given period
+ * 
+ * @param timer - Timer (1 or 2)
+ * @param ms - period given in miliseconds
  */
 void tmr_setup_period(int timer, int ms);
 
+/**
+ * @brief Wait until the given timer raises the flag
+ *
+ * @param timer - TIMER1 or TIMER2
+ * @return int - Returns 1 if flag is already raised, otherwise 0;
+ */
 void tmr_wait_period(int timer);
 
-
-
+/**
+ * @brief Set up the timer for given period and immediately wait for that period
+ * 
+ * @param timer 
+ * @param ms - the period
+ */
+void tmr_wait_ms(int timer, int ms)
 
 #endif	/* XC_HEADER_TEMPLATE_H */
 
