61526595016376b3102b92963f279df02905cb76
Michał <mkrepa117@outlook.com>
2024-04-15 22:10:35 +0200

    modify timer.c

--- a2_Timers.X/timer.c
+++ a2_Timers.X/timer.c
@@ -12,27 +12,30 @@
 void tmr_setup_period(int timer, int ms) {
     long clock = 72000000;      // 72 MHz
     int ps_mode = 0;            // Mode of prescaler 0, 1, 2, 3
-    long new_clock;             // Prescaled clock
-
+    int prescaler = 1;          // Value of prescaler
+    long new_clock = ;          // Prescaled clock
     /* check if we need to use prescaler */
     if ((float)ms * clock > 65535) // Check if the number of steps is within thershold of 16bit
     {
         // too many steps, use prescaler
-        new_clock = clock / 8; // 1:8 
+        prescaler = 8;  // 1:8 
+        new_clock = clock / prescaler; 
         ps_mode = 1;
         if ((float)ms * new_clock > 65535) // Check again
         {
-            new_clock = clock / 16; // 1:16
+            prescaler = 64; // 1:64
+            new_clock = clock / prescaler; 
             ps_mode = 2;
             if ((float)ms * new_clock > 65535) //and again
             {
-                new_clock = clock / 256; // 1:256
+                prescaler = 256;    // 1:256
+                new_clock = clock / prescaler; 
                 ps_mode = 3;
             }
         }
     }
     /* Set the value for the timer period */
-    long result = new_clock *((float) ms / 1000); // value of the timer
+    long result = new_clock * ( (float) ms / 1000); // value of the timer
     if (timer == TIMER1)
     {
         T1CONbits.TON = 0;
@@ -56,6 +59,8 @@ void tmr_setup_period(int timer, int ms) {
         {
             T1CONbits.TCKPS = 0;
         }
+        //IFS0bits.T1IF = 0; // clear TIMER1 interrupt flag
+        //IEC0bits.T1IE = 1; // enable TIMER1 interrupt
         /* Start the timer*/
         T1CONbits.TON = 1; 
     }   
@@ -83,6 +88,8 @@ void tmr_setup_period(int timer, int ms) {
         {
             T2CONbits.TCKPS = 0; 
         }
+        IFS0bits.T2IF = 0; // clear TIMER1 interrupt flag
+        IEC0bits.T2IE = 1; // enable TIMER1 interrupt
         /* Start the timer*/
         T2CONbits.TON = 1;
     }
@@ -98,13 +105,11 @@ int tmr_wait_period(int timer) {
             IFS0bits.T1IF = 0;
             return 1;
         }
-        T1CONbits.TON = 1; // Start the timer
         // Wait until flag is raised
         while (IFS0bits.T1IF == 0)
         {
         }
-//        IFS0bits.T1IF = 0; // zero the flag from 1 to 0 again.
-        return 0;
+        IFS0bits.T1IF = 0; // zero the flag from 1 to 0 again.
     }
     else if (timer == TIMER2)
     {
@@ -119,9 +124,9 @@ int tmr_wait_period(int timer) {
         while (IFS0bits.T2IF == 0)
         {
         }
-//        IFS0bits.T2IF = 0;  // zero the flag from 1 to 0 again.
-        return 0;
+        IFS0bits.T2IF = 0;  // zero the flag from 1 to 0 again.
     }
+    return 0;
 }
 
 
