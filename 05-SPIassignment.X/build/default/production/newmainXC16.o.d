d83073b2c5e62246b22884dab28c47d69d8f9aea
Romina Contreras Zerega <146995126+RominaZe@users.noreply.github.com>
2024-04-27 14:35:33 +0200

    SPI assignment
    first exercise is done the second i have to correct the values of the magnetometer because they gives me crazy values D:

---
+++ 05-SPIassignment.X/build/default/production/newmainXC16.o.d
@@ -0,0 +1,8 @@
+build/default/production/newmainXC16.o: newmainXC16.c function.h uart.h \
+ timer.h
+
+function.h:
+
+uart.h:
+
+timer.h:
---
+++ 05-SPIassignment.X/build/default/production/newmainXC161.o.d
@@ -0,0 +1,3 @@
+build/default/production/newmainXC161.o: newmainXC161.c timer.h
+
+timer.h:
