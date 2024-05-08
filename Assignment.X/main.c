5fab5f14b5315063d01db59a195ae33e26c8343f
romi-contreras <146995126+RominaZe@users.noreply.github.com>
2024-05-08 14:54:25 +0200

    Update main.c

--- Assignment.X/main.c
+++ Assignment.X/main.c
@@ -14,30 +14,44 @@
 #include <string.h>
 #include <math.h>
 
-// TIMER2 AND TIMER 3 are use in the spi and algorithm
+#define SPI_READ 4
+#define UART_SEND 20
 
-void magneticNORTH(int x_avg, int y_avg) {
-    double north;
-    char buffer[10];
-    north = atan2(y_avg, x_avg);
-    north = north * (180.0 / M_PI);
-    sprintf(buffer, "$YAW, %d* \0", (int) north);
-    for (int j = 0; buffer[j] != '\0'; j++) {
-        while (U1STAbits.UTXBF == 1);
-        U1TXREG = buffer[j];
-    }
-}
+int itr;
+char uart_msg[80]; 
+
+int is_msg_ready; // Flag to create a new uart_msg
+
+//// Interrupt routine associated to the receivement of message on the UART
+//void __attribute__((__interrupt__, __auto_psv__)) _U1TXInterrupt(){
+//    IFS0bits.U1TXIF = 0;
+//    U1TXREG = uart_msg[itr];
+//    itr++;
+//    if (uart_msg[itr] == '\0') {
+//        itr = 0;
+//        is_msg_ready = 1;
+//    }
+//}
 
 /**
  * Use to simulate an algorithm that takes 7 ms to be completed
  * @param None
  * @return None
  */
-void algortihm(){
-    tmr_setup_period(TIMER3, 7);
+void algorithm(){
+    tmr_setup_period(TIMER4, 7);
 }
 
-int16_t axis(char type){
+
+double get_magnetic_north(int x_avg, int y_avg) {
+    double north;
+    north = atan2(y_avg, x_avg);
+    north = north * (180.0 / M_PI);
+    return north;
+}
+
+
+int read_axis(char type){
     int16_t mag_LSB,mag_MSB,value;  
     CS3 = 0;
     
@@ -56,9 +70,8 @@ int16_t axis(char type){
     mag_MSB = spi_write(0x00); // read the value of the MSB
 
     // put the LSB and MSB together in a two byte signed integer
-
     mag_LSB = mag_LSB & 0b11111000; // clear the three least significant 
-    // bits by ANDing with a mask
+                                    // bits by ANDing with a mask
 
     mag_MSB = mag_MSB << 8; // left-shift by 8 the MSB
 
@@ -69,50 +82,107 @@ int16_t axis(char type){
     return (int)value;
 }
 
+void __attribute__((__interrupt__, __auto_psv__))_INT1Interrupt(){
+    IFS1bits.INT1IF = 0;    // clear the interrupt flag
+    // case 1 
+    // uart write
+    if (uart_msg[itr] == '\0') {
+        // message is finished, zero all values and set flag to 0
+        itr = 0;
+        is_msg_ready = 0;
+    }
+    else  {
+        // send one char and increment iterator
+        U1TXREG = uart_msg[itr];
+        itr++;
+    }
 
-int main() {
+    toggleLed(1);
+}
+
+int assignment() {
+    int uart_cnt = 0;   // counter to write on uart
+    int spi_cnt = 0;    // counter to read from spi
+    
+    int buffer_x[5] = {0};
+    int buffer_y[5] = {0};
+    int buffer_z[5] = {0};
+    int x_avg = 0, y_avg = 0, z_avg =0;
+    
+    uart_msg[0] = 'a';      //  initialize message for uart with some value
+    itr = 0;                // init iterator for uart_msg
+    is_msg_ready = 0;       // Set the flag to prepare uart messages
+
+
+    tmr_setup_period(TIMER1,10);        // 10 hz 
+    
+    while(1){
+        algorithm();
+
+        /* read from spi */
+        if(spi_cnt == SPI_READ){        // triggers every 40ms
+            if(spi_cnt < 5){
+                // fill up buffers
+                buffer_x[spi_cnt] = read_axis('x');
+                buffer_y[spi_cnt] = read_axis('y');
+                buffer_z[spi_cnt] = read_axis('z');
+            }
+            else {
+                // By doing this we skip one period when we could read
+                spi_cnt = 0;
+            
+            }
+        }
+
+        /* prepare message for uart */
+        if(uart_cnt == UART_SEND && !is_msg_ready){      //ideally triggers every 200 ms
+            //calculate MAG and YAW
+            x_avg = (buffer_x[0] + buffer_x[1] + buffer_x[2] + buffer_x[3] + buffer_x[4]) / 5.0;
+            y_avg = (buffer_y[0] + buffer_y[1] + buffer_y[2] + buffer_y[3] + buffer_y[4]) / 5.0;
+            z_avg = (buffer_z[0] + buffer_z[1] + buffer_z[2] + buffer_z[3] + buffer_z[4]) / 5.0;
+            int north = get_magnetic_north(x_avg,y_avg);
+            // set the message        
+             memset(uart_msg, 0, sizeof(uart_msg));
+             sprintf(uart_msg, "$MAG,%d,%d,%d* $YAW,%d", x_avg, y_avg, z_avg, north);
+            
+            // allow for sending message
+            is_msg_ready = 1;
+        }
+        
+        //increment counters
+        spi_cnt++;
+        uart_cnt++;
+
+        // check whether deadline is missed 
+        while(is_msg_ready && get_timer_status(TIMER1) == 0) {
+            // trigger the interrupt to write on uart
+            IFS1bits.INT1IF = 1;
+        }
+        // wait for the remainder of time;
+        tmr_wait_period(TIMER1);
+    }
+    return 0;
+}
+
+
+int main(){
     
     initializeIO();
     initUART();
     initSPI();
-  
+        
+    INTCON2bits.GIE = 1;    // set global interrupt enable
+    IFS1bits.INT1IF = 0;    // clear the interrupt flag
+    IEC1bits.INT1IE = 1;    // enable interrupt
+
     // Pass from the suspend mode of the magnetometer to the sleep and then 
     // to the active mode
     setACTIVEmode('M');
     
-    //int16_t x,y,z;
-    int16_t x_avg,y_avg,z_avg;
+    // 25Hz rate of the magnetometer 
+    spi_write(0x4C);
+    spi_write(0b00110000);
     
-    tmr_setup_period(TIMER1,10);
-    
-    
-    char buffer[50];
-    
-    while(1){
-        
-        x_avg = 0;
-        y_avg = 0;
-        z_avg = 0;
-        for(int p = 0; p<5; p++){
-            x_avg = axis('x') + x_avg;
-            y_avg = axis('y') + y_avg;
-            z_avg = axis('z') + z_avg;
-            tmr_wait_ms(TIMER4,100);
-        }
-        
-        x_avg = x_avg/5;
-        y_avg = y_avg/5;
-        z_avg = z_avg/5;
-        
-        sprintf(buffer, "$MAG, %d, %d, %d*  \0", (int16_t)x_avg,(int16_t)y_avg,(int16_t)x_avg);
-        for (int i = 0; buffer[i] != '\0'; i++) {
-            while (U1STAbits.UTXBF == 1);
-            U1TXREG = buffer[i];
-        }
-        
-        magneticNORTH(x_avg,y_avg);
-        
-    }
-     
-    return 0;
+    assignment();
 }
+
