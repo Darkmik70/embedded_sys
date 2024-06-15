b37ad599142c71c887507e00b64cb79f65f84ce4
Michał <mkrepa117@outlook.com>
2024-06-15 12:39:23 +0200

    Add CommandList to the Project This commit contains struct of CommandList, a fifo that will be responsible for storing commands for PWM. DISCLAIMER I HAD NO CHANCE TO TEST IT, THERE MAY BE SOME MISTAKES

---
+++ Project.X/command_list.h
@@ -0,0 +1,62 @@
+#ifndef COMMAND_LIST_H
+#define	COMMAND_LIST_H
+
+#define CMD_LIST_SIZE 10
+
+enum msg_type{
+    Forward = 1,
+    CounterClockwise = 2,
+    Clockwise = 3,
+    Backward = 4}
+
+typedef struct {
+    int type[CMD_LIST_SIZE];
+    int time[CMD_LIST_SIZE];
+    int size;
+} CommandList;
+
+/**
+ * @brief Initialize command list with zeros
+ * 
+ * @param cmd_list ptr to CommandList struct
+ */
+void init_cmd_list(CommandList *cmd_list); 
+
+/**
+ * @brief checks if the command list is full
+ *
+ * @param cmd_list ptr to CommandList struct
+ * @return (int) 1 if fifo is full, 0 otherwise
+ */
+int is_cmd_list_full(CommandList *cmd_list); 
+
+/**
+ * @brief checks if command list is empty
+ *
+ * @param cmd_list
+ * @return (int) 1 if fifo is empty, 0 otherwise
+ */
+int is_cmd_list_empty(CommandList *cmd_list); 
+
+/**
+ * @brief Add element at the end of the CommandList queue
+ *
+ * @param cmd_list  ptr to Command List
+ * @param cmd_type  (int) Type of command
+ * @param cmd_time  (int) duration of the command
+ * @return (int) 1 if success, 0 if full
+ */
+int enqueue_cmd_list(CommandList *cmd_list, int cmd_type, int cmd_time);
+
+/**
+ * @brief Remove the first element from CommandList fifo
+ * 
+ * @param cmd_list ptr to CommandList struct
+ * @param cmd_type (int) type of command
+ * @param cmd_time (int) time duration of command
+ * @return int 1 if success, 0 if fifo is empty
+ */
+int dequeue_cmd_list(CommandList *cmd_list, int &cmd_type, int &cmd_time);
+
+
+#endif /* COMMAND_LIST_H */
